dae28eedce3964b8afe7bacd52f7d8902129a0f7
漂泊80 <13671172337@163.com>
2017-08-03 09:56:10 +0800

    nodejs 06

--- node/lesson06.js
+++ node/lesson06.js
@@ -9,5 +9,43 @@
 console.log(process.pid);
 console.log(process.version);
 console.log(process.platform);
+console.log(process.title);
+console.log(process.argv);
+console.log('~~~~~~~~~~~~~~~~~~~~');
+var argv = process.argv.length;
+if(argv >= 3){
+	for(var i = 0 ; i < argv;i++){
+		if (i > 1){
+			console.log(process.argv[i])
+		}
+	}
+}
+
+
+// console.log(process.execPath);
+// console.log(process.env);
+
+process.on('exit',function(){
+	console.log('now node exit!!!');
+})
+
+
+process.on('uncaughtException', function(err) {
+	console.log("uncaughtException called ", err);
+});
+
+// process.chdir('/Users/bingbing/Documents/boyichuangwei');
+console.log(process.cwd());
+
+
+process.nextTick(function(){
+	console.log('nextTick')
+});
+
+notfunction_text();
+console.log('after notfunction_text')
+
+
+
 
 
